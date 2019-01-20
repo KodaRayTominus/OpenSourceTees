6f4d6cc3908d3d3c9f6b72bc61d5b2c452b43eaf
Dakota Ray Tominus <koda.tominus@hotmail.com>
2019-01-19 18:24:44 -0800

    Add project files.

---
+++ OpenSourceTees/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenSourceTees.MvcApplication" Language="C#" %>
---
+++ OpenSourceTees/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenSourceTees.Startup))]
+namespace OpenSourceTees
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
