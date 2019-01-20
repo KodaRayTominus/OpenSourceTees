0bf5f4f84fa4e611f0b7247f44edd2a89188684a
Dakota Ray Tominus <koda.tominus@hotmail.com>
2019-01-19 18:48:00 -0800

    Set up user roles

---
+++ OpenSourceTees/Migrations/201901200235531_initial.cs
@@ -0,0 +1,16 @@
+namespace OpenSourceTees.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
