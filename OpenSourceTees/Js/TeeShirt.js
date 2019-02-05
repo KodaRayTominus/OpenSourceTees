9f3f0267d3db797635f47bbdcdefe673c460217b
Dakota Ray Tominus <koda.tominus@hotmail.com>
2019-02-04 22:20:49 -0800

    implemented canvas for blend4web, tested works and loads with temp placement tee

--- OpenSourceTees/Js/TeeShirt.js
+++ OpenSourceTees/Js/TeeShirt.js
@@ -1,64 +1,85 @@
 ﻿"use strict"
 
-import b4w from "blend4web";
+b4w.register("TeeShirt_main", function (exports, require) {
 
-var m_app = b4w.app;
-var m_data = b4w.data;
-var m_scs = b4w.scenes;
-var m_cfg = b4w.config;
-var m_quat = b4w.quat;
-var m_armat = b4w.armature;
-var m_tsr = b4w.tsr;
-var m_phy = b4w.physics;
-var m_trans = b4w.transform;
-var m_vec3 = b4w.vec3;
-var m_util = b4w.util;
-var m_cam = b4w.camera;
-var m_version = b4w.version;
 
-var DEBUG = (m_version.type() === "DEBUG");
+    var m_app = require("app");
+    var m_cfg = require("config");
+    var m_data = require("data");
+    var m_preloader = require("preloader");
+    var m_ver = require("version");
 
-export function init() {
-    m_app.init({
-        canvas_container_id: "main_canvas_container",
-        callback: init_cb,
-        physics_enabled: false,
-        alpha: true,
-        show_fps: true,
-        autoresize: true,
-        assets_dds_available: !DEBUG,
-        assets_min50_available: !DEBUG,
-        console_verbose: true
-    });
-}
+    var DEBUG = (m_ver.type() == "DEBUG");
+    var APP_ASSETS_PATH = m_cfg.get_std_assets_path();
 
-function init_cb(canvas_elem, success) {
 
-    if (!success) {
-        console.log("b4w init failure");
-        return;
+    exports.init = function() {
+        m_app.init({
+            canvas_container_id: "main_canvas_container",
+            callback: init_cb,
+            show_fps: true,
+            autoresize: true,
+            assets_dds_available: !DEBUG,
+            assets_min50_available: !DEBUG,
+            console_verbose: true
+        });
     }
-    load();
-}
 
-function load() {
-    m_data.load(APP_ASSETS_PATH + "IMAGE", load_cb);
-}
+    function init_cb(canvas_elem, success) {
 
-function load_cb(data_id) {
-    m_app.enable_camera_controls(false, false, false, null, true);
-    var container = m_cont.get_canvas();
-    _world = m_scenes.get_world_by_name("World");
-}
-
-function main_canvas_clicked_cb(e) {
-
-    var x = m_mouse.get_coords_x(e);
-    var y = m_mouse.get_coords_y(e);
-
-    var obj = m_scenes.pick_object(x, y);
-    if (obj) {
+        if (!success) {
+            console.log("b4w init failure");
+            return;
+        }
+        load();
     }
-}
 
-init();
+    function load() {
+
+        var preloader_cont = document.getElementById("preloader_cont");
+        m_data.load("./TeeShirtToExport.json", load_cb);
+    }
+    
+    function preloader_cb(percentage) {
+        var prelod_dynamic_path = document.getElementById("prelod_dynamic_path");
+        var percantage_num = prelod_dynamic_path.nextElementSibling;
+
+        prelod_dynamic_path.style.width = percentage + "%";
+        percantage_num.innerHTML = percentage + "%";
+        if (percentage == 100) {
+            var preloader_cont = document.getElementById("preloader_cont");
+            preloader_cont.style.visibility = "hidden";
+            return;
+        }
+    }
+
+    function load_cb(data_id, success) {
+        if (!success) {
+            console.log("b4w load failure");
+            return;
+        }
+        m_app.enable_camera_controls(false, false, false, null, true);
+        //load_data();
+    }
+
+    function load_data() {
+        var shirt = m_scenes.get_object_by_name("shirt");
+        var ctx_image = m_tex.get_canvas_ctx(shirt, "tex_canvas");
+
+        if (ctx_image) {
+            var img = new Image();
+            img.src = APP_ASSETS_PATH + "earth.jpg";
+            img.onload = function () {
+                ctx_image.drawImage(img, 0, 0, ctx_image.canvas.width,
+                    ctx_image.canvas.height);
+                m_tex.update_canvas_ctx(shirt, "tex_canvas");
+            }
+        }
+    }
+
+    function create_interface(obj) {
+
+    }
+});
+
+b4w.require("TeeShirt_main").init();
