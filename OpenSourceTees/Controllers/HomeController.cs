7088c501dfb899051c745e3f3283c80ed5986236
Dakota Ray Tominus <koda.tominus@hotmail.com>
2019-03-04 18:02:31 -0800

    changed contact page to partial view

--- OpenSourceTees/Controllers/HomeController.cs
+++ OpenSourceTees/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace OpenSourceTees.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return PartialView();
         }
 
         public ActionResult Home()
