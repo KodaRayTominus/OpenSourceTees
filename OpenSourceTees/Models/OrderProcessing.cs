3fca7957e9a6e3cf1fdac580745e1f6392bf0385
Dakota Ray Tominus <koda.tominus@hotmail.com>
2019-03-13 12:04:56 -0700

    went through and commented up fields, methods, and helpers

--- OpenSourceTees/Models/OrderProcessing.cs
+++ OpenSourceTees/Models/OrderProcessing.cs
@@ -7,34 +7,74 @@ using System.Web;
 
 namespace OpenSourceTees.Models
 {
+    /// <summary>
+    /// Object representation of the Order Process
+    /// </summary>
     public class OrderProcessing
     {
+        /// <summary>
+        /// Primary key of the Order
+        /// </summary>
         [Key]
         [ForeignKey("Order")]
         public string OrderId { get; set; }
 
+        /// <summary>
+        /// date the order was created
+        /// </summary>
         [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public string CreateDate { get; set; }
 
+        /// <summary>
+        /// boolean representation of emails being sent
+        /// </summary>
         public bool IsEmailSent { get; set; }
 
+        /// <summary>
+        /// boolean representation of order being accepted
+        /// </summary>
         public bool IsAccepted { get; set; }
 
+        /// <summary>
+        /// boolean representation of order being processed
+        /// </summary>
         public bool IsProcessed { get; set; }
 
+        /// <summary>
+        /// String Id of the processor handling the order, foreign key
+        /// </summary>
         [ForeignKey("ApplicationUser")]
         public string ProcessorId { get; set; }
 
+        /// <summary>
+        /// boolean representation of order being shipped
+        /// </summary>
         public bool IsShipped { get; set; }
 
+        /// <summary>
+        /// boolean representation of order being delivered
+        /// </summary>
         public bool IsDelivered { get; set; }
 
+        /// <summary>
+        /// boolean representation of order being canceled
+        /// </summary>
         public bool IsCanceled { get; set; }
 
+        /// <summary>
+        /// foreign key application user of processor
+        /// </summary>
         public virtual ApplicationUser ApplicationUser { get; set; }
 
+        /// <summary>
+        /// foreign key order of the OrderId
+        /// </summary>
         public virtual PurchaseOrder Order { get; set; }
 
+        /// <summary>
+        /// gets and returns the Processors UserName
+        /// </summary>
+        /// <returns>UserName of the processor</returns>
         public string GetProcessorUserName()
         {
             ApplicationDbContext db = new ApplicationDbContext();
--- OpenSourceTees/Models/TeeShirtUploadViewModel.cs
+++ OpenSourceTees/Models/TeeShirtUploadViewModel.cs
@@ -5,9 +5,19 @@ using System.Web;
 
 namespace OpenSourceTees.Models
 {
+    /// <summary>
+    /// Represents the modle for uploading a TeeShirt image
+    /// </summary>
     public class TeeShirtUploadViewModel
     {
+        /// <summary>
+        /// file to be uploaded
+        /// </summary>
         public HttpPostedFileBase File { get; set; }
+
+        /// <summary>
+        /// Image object to be palced in the db
+        /// </summary>
         public Image Image { get; set; }
     }
 }
